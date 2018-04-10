1f4a5ae4f3d760da3c3c7667e0444dea817627d0
shenzhenMirren <690389491@qq.com>
2018-04-10 15:44:20 +0800

    修改为druid数据源,完善修改静态js与css在本地获取,以便依没有网络时可以正常使用

---
+++ src/main/java/com/szmirren/vxApi/core/common/DataSourceProviderOfDruid.java
@@ -0,0 +1,39 @@
+package com.szmirren.vxApi.core.common;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.alibaba.druid.filter.logging.Log4j2Filter;
+import com.alibaba.druid.pool.DruidDataSource;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.spi.DataSourceProvider;
+
+public class DataSourceProviderOfDruid extends DruidDataSource implements DataSourceProvider {
+	private static final long serialVersionUID = 381971380983136762L;
+
+	public int maximumPoolSize(DataSource dataSource, JsonObject config) throws SQLException {
+		return config.getInteger("max_pool_size", 15).intValue();
+	}
+
+	public DataSource getDataSource(JsonObject config) throws SQLException {
+		Log4j2Filter log4j2 = new Log4j2Filter();
+		DruidDataSource ds = new DruidDataSource();
+		ds.getProxyFilters().add(log4j2);
+		ds.setUrl(config.getString("url"));
+		ds.setDriverClassName(config.getString("driver_class"));
+		ds.setUsername(config.getString("user"));
+		ds.setPassword(config.getString("password"));
+		ds.setMaxActive(config.getInteger("max_pool_size", 15));
+		ds.setMinIdle(config.getInteger("min_pool_size", 1));
+		ds.setInitialSize(config.getInteger("initial_pool_size", 1));
+		ds.setMinEvictableIdleTimeMillis(config.getLong("max_idle_time", 60000L));
+		return ds;
+	}
+
+	public void close(DataSource dataSource) throws SQLException {
+		if ((dataSource instanceof DataSourceProviderOfDruid))
+			((DataSourceProviderOfDruid) dataSource).close();
+	}
+}
--- src/main/resources/templates/updateAPP.ftl
+++ src/main/resources/templates/updateAPP.ftl
@@ -6,13 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="webkit|ie=edge">
     <title>VX-API-UPDATE-APP</title>
-    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-          crossorigin="anonymous">
-    <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"
-            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/static/framework/bootstrap.min.css">
+    <script src="/static/framework/jquery.min.js"></script>
+    <script src="/static/framework/bootstrap.min.js"></script>
     <link rel="stylesheet" href="/static/css/main.css">
 </head>
 
