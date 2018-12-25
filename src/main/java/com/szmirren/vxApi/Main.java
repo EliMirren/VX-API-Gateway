d68771c9d98b0c4e187b86bc3c168318dd55e774
690389491@qq.com <miniand3>
2018-12-25 15:42:55 +0800

    添加一个开发环境可以使用的Main测试类

---
+++ src/main/java/com/szmirren/vxApi/Main.java
@@ -0,0 +1,15 @@
+package com.szmirren.vxApi;
+
+import com.szmirren.vxApi.core.VxApiLauncher;
+
+/**
+ * 该类只是开发环境启动使用,没有什么实际的用处
+ * 
+ * @author <a href="http://szmirren.com">Mirren</a>
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		VxApiLauncher.start();
+	}
+}
